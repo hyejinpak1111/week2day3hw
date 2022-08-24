7be72a87b5b7b75187b2e377ef46668a6185280d
Juneun Park <ndnism28@gmail.com>
2022-08-24 15:11:23 -0700

    first commit

---
+++ day3hw.py
@@ -0,0 +1,17 @@
+# Area of a house
+
+def area(length, width):
+    return length * width
+
+print(area(2,3))
+
+
+
+
+from math import pi
+
+def cir(r): #take in radius, or diameter
+    # enter formula for circumference from radius or diameter here
+    return 2 * pi * r
+
+print(cir(6))
